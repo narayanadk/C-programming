879757c7933fa66a726bc3b00383555f77f826a1
Narayan Adhikari <68802345+narayanadk@users.noreply.github.com>
2021-10-28 08:40:14 +0545

    Update strrev.cpp

--- strrev.cpp
+++ strrev.cpp
@@ -5,7 +5,7 @@ int main()
 	char name[100],rev[100];
 	printf("enter any string\t");
 	gets(name);
-    strrev(name);//reverse the string
+    	strrev(name);//reverse the string
 	printf("\nthe reverse string is %s",name);
 	return 0;
 }
